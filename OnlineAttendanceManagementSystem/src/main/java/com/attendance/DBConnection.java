38f4af2573f48f65eaf360be1cf98f9f8f7df210
chinchankar25 <chinchankarkomal25@gmail.com>
2026-08-23 18:57:31 +0530

    Update DBConnection.java

--- OnlineAttendanceManagementSystem/src/main/java/com/attendance/DBConnection.java
+++ OnlineAttendanceManagementSystem/src/main/java/com/attendance/DBConnection.java
@@ -6,16 +6,17 @@ import java.sql.DriverManager;
 public class DBConnection {
 
     public static Connection getConnection() {
+
         Connection con = null;
 
         try {
             Class.forName("org.postgresql.Driver");
 
             String url = System.getenv("DB_URL");
-            String user = System.getenv("DB_USER");
+            String username = System.getenv("DB_USERNAME");
             String password = System.getenv("DB_PASSWORD");
 
-            con = DriverManager.getConnection(url, user, password);
+            con = DriverManager.getConnection(url, username, password);
 
             System.out.println("Database Connected Successfully!");
 
